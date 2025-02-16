bde30613b89a7679b6c051ac81b1b5eda913aff0
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-02-16 17:51:21 +0600

    added funtion and ui

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:expense_app/model/expense.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatefulWidget {
@@ -8,12 +9,103 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  final List<Expense> _expense = [];
+  final List<String> _category = [
+    'Food',
+    'Transport',
+    'Entertainment',
+    'Bills'
+  ];
+  double _total = 0.0;
+
+  void _showForm(BuildContext context) {
+    TextEditingController titleController = TextEditingController();
+    TextEditingController amountController = TextEditingController();
+
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (_) {
+        return Padding(
+          padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+              left: 16,
+              right: 16,
+              top: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: "Title"),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "Amount"),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+ // 1 hour.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Expense Tracker"),
-        // 33 min
+        actions: [
+          IconButton(
+            onPressed: () => _showForm(context),
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Card(
+              margin: EdgeInsets.all(16),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  "Total: \$$_total",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _expense.length,
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.blueAccent,
+                      child: Text(_expense[index].category),
+                    ),
+                    title: Text(_expense[index].title),
+                    subtitle: Text(
+                      _expense[index].date.toString(),
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showForm(context),
+        child: Icon(Icons.add),
       ),
     );
   }
